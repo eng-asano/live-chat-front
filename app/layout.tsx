6dff04785797bdf8f14595f14796663d6986c55e
eng-asano <eng.y.asano@gmail.com>
2024-10-05 21:06:45 +0900

    feat: Pandaの初期設定

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,20 @@
+import { Fira_Code } from 'next/font/google'
+import './globals.css'
+
+export const FiraCode = Fira_Code({
+  weight: ['500', '700'],
+  display: 'swap',
+  subsets: ['latin'],
+  variable: '--font-fira-code',
+})
+
 interface Props {
   children: React.ReactNode
 }
 
 export default function RootLayout({ children }: Readonly<Props>) {
   return (
-    <html lang="en">
+    <html lang="en" className={FiraCode.variable}>
       <body>{children}</body>
     </html>
   )
--- panda.config.ts
+++ panda.config.ts
@@ -1,6 +1,24 @@
-import { defineConfig } from '@pandacss/dev'
+import { defineConfig, defineGlobalStyles } from '@pandacss/dev'
+
+const globalCss = defineGlobalStyles({
+  'html, body': {
+    fontSize: '16px',
+    letterSpacing: '0.1rem',
+    color: 'gray.800',
+    lineHeight: '1.5',
+    background: 'background',
+  },
+  html: {
+    '--global-font-body': 'Roboto, sans-serif',
+  },
+  h2: {
+    fontFamily: 'Oswald',
+  },
+})
 
 export default defineConfig({
+  globalCss,
+
   // Whether to use css reset
   preflight: true,
 
@@ -19,6 +37,27 @@ export default defineConfig({
         lg: '1024px',
         xl: '1280px',
         '2xl': '1536px',
+      },
+      tokens: {
+        colors: {
+          primary: {
+            main: { value: '#0891b2' },
+          },
+          background: { value: '#fff' },
+        },
+        fonts: {
+          fira: { value: 'var(--font-fira-code)' },
+        },
+        shadows: {
+          neumorphism: {
+            dent: {
+              value: 'inset 2px 2px 5px #babecc, inset -4px -4px 10px #ffffff73',
+            },
+            bump: {
+              value: '2px 2px 5px #babecc, -4px -4px 10px #ffffff73',
+            },
+          },
+        },
       },
     },
   },
