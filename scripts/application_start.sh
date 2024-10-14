24b18666bd7e7389dd899e0d8f4f723cba2c7e25
eng-asano <eng.y.asano@gmail.com>
2024-10-14 20:34:52 +0900

    chore: install pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,6 +2,14 @@
 
 cd /var/www/live-chat
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+
+nvm install 20.17.0
+
+npm install -g yarn
+yarn install
+
 npm install -g pm2
 pm2 start yarn --name "live-chat" -- start
 pm2 save
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,5 +2,11 @@
 
 cd /var/www/live-chat
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+
+nvm install 20.17.0
+
+npm install -g pm2
 pm2 stop live-chat || true
 pm2 delete live-chat || true
