02fb4fe36dab560381448e74b70dfb3854b54999
eng-asano <eng.y.asano@gmail.com>
2024-10-14 22:58:36 +0900

    chore: cd

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,20 +1,6 @@
 #!/bin/bash
 
-cd /var/www/live-chat
+cd /var/www/live-chat && \
 
-# export NVM_DIR="$HOME/.nvm"
-# [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-# [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
-
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# source ~/.bashrc
-
-# nvm install 20.17.0
-
-# npm install -g yarn
-# yarn install
-
-# npm install -g pm2
-
-~/.nvm/versions/node/v20.18.0/bin/pm2 start yarn --name "live-chat" -- start
-~/.nvm/versions/node/v20.18.0/bin/pm2 save
+m2 start yarn --name "live-chat" -- start && \
+pm2 save
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -1,13 +1,6 @@
 #!/bin/bash
 
-# cd /var/www/live-chat
+cd /var/www/live-chat && \
 
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-# source ~/.bashrc
-
-# nvm install 20.17.0
-
-# npm install -g pm2
-
-~/.nvm/versions/node/v20.18.0/bin/pm2 stop live-chat || true
-~/.nvm/versions/node/v20.18.0/bin/pm2 delete live-chat || true
+pm2 stop live-chat || true && \
+pm2 delete live-chat || true
