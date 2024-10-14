be9b49d1584381e5720b697e2a00bf02e0e9f149
eng-asano <eng.y.asano@gmail.com>
2024-10-14 19:53:52 +0900

    chore: codebuild

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,15 +2,6 @@
 
 cd /var/www/live-chat
 
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
-source ~/.bashrc
-
-nvm install 20.17.0
-
-npm install -g yarn
-yarn install
-# yarn build
-
-# npm install -g pm2
-# pm2 start yarn --name "live-chat" -- start
-# pm2 save
+npm install -g pm2
+pm2 start yarn --name "live-chat" -- start
+pm2 save
