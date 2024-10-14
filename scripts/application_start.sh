93583b257e473f873cf2fbab1791a0eec1cdf06d
eng-asano <eng.y.asano@gmail.com>
2024-10-14 18:15:57 +0900

    chore: install

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# アプリケーションのディレクトリに移動
-cd /var/www/live-chat || exit
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
 
+nvm install 20.17.0
 node -v
-# npm install -g yarn
+
+npm install -g yarn
+yarn -v
 # yarn install
 # yarn build
 # pm2 start yarn --name "live-chat" -- start
