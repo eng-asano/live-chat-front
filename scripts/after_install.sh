f9093ed64c9d0e29c44479633e979048f34d0f9c
eng-asano <eng.y.asano@gmail.com>
2024-10-16 00:07:20 +0900

    chore: cd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd /var/www/live-chat
+# cd /var/www/live-chat
 
-/usr/bin/pm2 start yarn --name "live-chat" -- start
+/usr/bin/pm2 start /usr/bin/yarn --name "live-chat" -- start
 /usr/bin/pm2 save
