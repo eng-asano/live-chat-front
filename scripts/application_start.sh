e5203aa28737c1ddd37355d9f7257f54b39609a5
eng-asano <eng.y.asano@gmail.com>
2024-10-15 23:27:25 +0900

    chore: /usr/bin/pm2

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 cd /var/www/live-chat
 
-~/.nvm/versions/node/v20.18.0/bin/pm2 start yarn --name "live-chat" -- start
-~/.nvm/versions/node/v20.18.0/bin/pm2 save
+/usr/bin/pm2 start yarn --name "live-chat" -- start
+/usr/bin/pm2 save
--- scripts/application_stop.sh
+++ scripts/application_stop.sh
@@ -2,5 +2,5 @@
 
 cd /var/www/live-chat
 
-~/.nvm/versions/node/v20.18.0/bin/pm2 stop live-chat || true
-~/.nvm/versions/node/v20.18.0/bin/pm2 delete live-chat || true
+/usr/bin/pm2 stop live-chat || true
+/usr/bin/pm2 delete live-chat || true
