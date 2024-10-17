1008aa566b1a499b55238734b958180ba68ca27f
eng-asano <eng.y.asano@gmail.com>
2024-10-17 18:05:26 +0900

    chore: discard-paths no

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# cd /var/www/live-chat
+cd /var/www/live-chat
 
-/usr/bin/pm2 start /usr/bin/yarn --name "live-chat" -- start
+/usr/bin/pm2 start /usr/bin/yarn --name "live-chat" -- start --cwd /var/www/live-chat
 /usr/bin/pm2 save
