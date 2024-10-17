af7c59fec926e72813bfdb02e734c4365e2f51e0
eng-asano <eng.y.asano@gmail.com>
2024-10-17 20:28:59 +0900

    chore: delete cmd

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -3,5 +3,5 @@
 cd /var/www/live-chat
 
 sudo unzip build.zip
-/usr/bin/pm2 start /usr/bin/yarn --name "live-chat" -- start --cwd /var/www/live-chat
+/usr/bin/pm2 start /usr/bin/yarn --name "live-chat" -- start
 /usr/bin/pm2 save
